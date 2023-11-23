b8c516676fdaa229a6f6a481228c1abb518b9b27
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-11-23 11:45:41 -0300

    Openning repositories

---
+++ lib/pages/start_page.dart
@@ -0,0 +1,166 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  bool _isLoading = true;
+  late final List<String> _recents;
+
+  @override
+  void initState() {
+    _loadRecents();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth >= 768) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('GitF'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                _menuContent,
+                const Padding(
+                  padding: EdgeInsets.only(right: 12.0),
+                  child: VerticalDivider(),
+                ),
+                Expanded(child: _recentsContent),
+              ],
+            ),
+          ),
+        );
+      } else {
+        return Scaffold(
+          drawer: Drawer(
+              child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 30.0),
+                child: Icon(Icons.menu),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: _menuContent,
+              ),
+            ],
+          )),
+          appBar: AppBar(
+            title: const Text('GitF'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: _recentsContent,
+          ),
+        );
+      }
+    });
+  }
+
+  Widget get _recentsContent => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 12.0),
+            child: Text(
+              'Recentes',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          if (_isLoading)
+            const Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(child: CircularProgressIndicator()),
+                ],
+              ),
+            )
+          else if (_recents.isEmpty)
+            const Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      'Nenhum repositório acessado recentemente',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          else
+            Expanded(
+              child: ListView(
+                children: _recents.reversed.map((e) {
+                  return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: ListTile(title: Text(e)),
+                  );
+                }).toList(),
+              ),
+            ),
+        ],
+      );
+
+  Widget get _menuContent => ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 300),
+        child: Column(
+          children: [
+            ListTile(
+              onTap: _newRepository,
+              leading: const Icon(Icons.add),
+              title: const Text('Criar um novo repositório'),
+            ),
+            const SizedBox(height: 16),
+            ListTile(
+              onTap: _openRepository,
+              leading: const Icon(Icons.open_in_browser),
+              title: const Text('Abrir um repositório existente'),
+            ),
+          ],
+        ),
+      );
+
+  Future<void> _saveRecents() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('Recents', _recents);
+  }
+
+  Future<void> _loadRecents() async {
+    if (!_isLoading) setState(() => _isLoading = true);
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _recents = prefs.getStringList('Recents') ?? [];
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _newRepository() async {}
+
+  Future<void> _openRepository() async {
+    final String? path = await FilePicker.platform.getDirectoryPath();
+    if (path != null) {
+      setState(() {
+        _recents.remove(path);
+        _recents.add(path);
+      });
+      await _saveRecents();
+    }
+  }
+}
